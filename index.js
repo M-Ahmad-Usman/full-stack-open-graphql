e7e6428b9ec8fed918a2225b8710ca6a53dfff86
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-08-16 02:11:05 +0500

    Format index.js using prettier.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
-const { randomUUID: uuid } = require('node:crypto')
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { randomUUID: uuid } = require("node:crypto");
 
 let persons = [
   {
@@ -8,28 +8,27 @@ let persons = [
     phone: "040-123543",
     street: "Tapiolankatu 5 A",
     city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
   },
   {
     name: "Matti Luukkainen",
     phone: "040-432342",
     street: "Malminkaari 10 A",
     city: "Helsinki",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
   },
   {
     name: "Venla Ruuska",
     street: "Nallemäentie 22 C",
     city: "Helsinki",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
   },
-]
+];
 
 const typeDefs = /* GraphQL */ `
-
   type Address {
     street: String!
-    city: String! 
+    city: String!
   }
 
   type Person {
@@ -53,39 +52,38 @@ const typeDefs = /* GraphQL */ `
       city: String!
     ): Person
   }
-`
+`;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
   Person: {
     address: (root) => {
       return {
         street: root.street,
-        city: root.city
-      }
-    }
+        city: root.city,
+      };
+    },
   },
   Mutation: {
     addPerson: (root, args) => {
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
-      return person
-    }
-  }
-}
+      const person = { ...args, id: uuid() };
+      persons = persons.concat(person);
+      return person;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-})
+});
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
 }).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  console.log(`Server ready at ${url}`);
+});
