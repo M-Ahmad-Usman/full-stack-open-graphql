e65dcd57347ae67a80424d7059add1877e62f1eb
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-08-19 04:23:02 +0500

    Implement error handling in client.
    - Create new Notify component to render errors.
    - Implement error handling for creating new person.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,31 @@
 import { useQuery } from "@apollo/client/react";
+import { useState } from "react";
 
 import { ALL_PERSONS } from "./queries";
 
 // Components
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
+import Notify from "./components/Notify";
 
 const App = () => {
   const result = useQuery(ALL_PERSONS);
+  const [errorMessage, setErrorMessage] = useState(null)
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
+  const notify = message => {
+    setErrorMessage(message)
+    setTimeout(() => { setErrorMessage(null) }, 10000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 };
--- client/src/components/PersonForm.jsx
+++ client/src/components/PersonForm.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client/react";
 import { CREATE_PERSON, ALL_PERSONS } from "../queries";
 
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [street, setStreet] = useState("");
   const [city, setCity] = useState("");
 
   const [createPerson] = useMutation(CREATE_PERSON, {
-    refetchQueries: [{ query: ALL_PERSONS }]
+    refetchQueries: [{ query: ALL_PERSONS }],
+    onError: (error) => setError(error.message)
   });
 
   const submit = (event) => {
