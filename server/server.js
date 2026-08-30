e37e716aacaec14a3dba1216090ddaff5a67167e
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-08-31 00:03:32 +0500

    Attached logged in user's to context object.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { jwt } = require("jsonwebtoken");
 
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
+const User = require("./models/user")
 
 const startServer = (port) => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
+  const server = new ApolloServer({ typeDefs, resolvers });
+
   startStandaloneServer(server, {
     listen: { port },
+    context: async ({ req }) => {
+      const auth = req.headers.authorization
+      const currentUser = await getUserFromAuthHeader(auth)
+      return { currentUser }
+    }
   }).then(({ url }) => {
     console.log(`Server ready at ${url}`);
   });
 };
 
-module.exports = startServer
+const getUserFromAuthHeader = async (auth) => {
+  if (!auth || !auth.startsWith('Bearer ')) return null
+
+  const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+  return User.findById(decodedToken.id).populate('friends')
+}
+
+module.exports = startServer;
