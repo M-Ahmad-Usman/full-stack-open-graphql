3339102f961de87a9e3cb79a06888cf9dc0e83de
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-09-11 10:34:18 +0500

    Write User mongoose model.

---
+++ library-backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const USERNAME_MIN_LENGTH = 3
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minLength: USERNAME_MIN_LENGTH
+  },
+  favoriteGenre: String
+})
+
+const User = mongoose.model('User', schema)
+
+module.exports = { User, USERNAME_MIN_LENGTH }
