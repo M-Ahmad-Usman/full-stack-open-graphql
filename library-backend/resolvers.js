0c26fc4c8a576c791286d9b503e579016f500203
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-09-11 14:35:44 +0500

    Update login password to the one expected in tests

--- library-backend/resolvers.js
+++ library-backend/resolvers.js
@@ -106,7 +106,7 @@ const resolvers = {
     login: async (root, args) => {
       const user = await User.findOne({ username: args.username })
 
-      if (!user || args.password !== 'secret123')
+      if (!user || args.password !== 'secret')
         throw new GraphQLError('wrong credentials', {
           extensions: { code: 'BAD_USER_INPUT' }
         })
